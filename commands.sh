46bec3357398e0c33171b5397ba0708defd27ed5
yishai <yishai5555@gmail.com>
2022-09-09 11:47:15 +0300

    01-flask

---
+++ commands.sh
@@ -0,0 +1,18 @@
+python3 -V # verify installed
+sudo apt install python3-venv -y
+
+mkdir flask_application && cd flask_application
+
+# create virtual env
+python3 -m venv venv
+source venv/bin/activate
+
+# pip install Flask
+pip install flask==1.1.2
+
+
+
+
+
+
+
